55bc615bf97fbf301962afd4ee4b12aba7796f4c
benchenas <97876328+benchenas@users.noreply.github.com>
2022-01-21 20:01:12 +0800

    Update main.py

--- algs/evocnn/main.py
+++ algs/evocnn/main.py
@@ -1,6 +1,12 @@
 import numpy as np
 import copy
 import os
+import sys
+
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(os.path.split(rootPath)[0])
+
 
 from compute import Config_ini
 from compute.file import get_algo_local_dir
